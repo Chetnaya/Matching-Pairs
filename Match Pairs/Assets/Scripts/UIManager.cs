91bb398434eddd106c749af121429b24b997b44b
Chetnaya Kalyan <chetnaya.kalyan@gmail.com>
2024-10-11 08:54:05 +0700

    feat: add UI manager

---
+++ Match Pairs/Assets/Scripts/UIManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject exitButton; 
+    [SerializeField] private GameObject instructionPanel;
+
+    void Start()
+    {
+        // Check the platform and toggle the exit button accordingly
+#if UNITY_STANDALONE_WIN
+            exitButton.SetActive(true);
+#else
+        exitButton.SetActive(false);
+#endif
+
+        // Initially hide the instruction panel
+        instructionPanel.SetActive(false);
+    }
+
+    // Call this method when the exit button is clicked
+    public void OnExitButtonClick()
+    {
+#if UNITY_STANDALONE_WIN
+            Application.Quit();
+#endif
+    }
+
+    // Call this method when the instructions button is clicked
+    public void OnInstructionsButtonClick()
+    {
+        instructionPanel.SetActive(true); // Show the instructions panel
+    }
+
+    // Call this method when the back button on the instruction panel is clicked
+    public void OnInstructionsBackBtnClick()
+    {
+        instructionPanel.SetActive(false); // Hide the instructions panel
+    }
+}
